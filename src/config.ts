1a0a8288b5c381341c925f5b7c8dc3f16aeb633b
Rafael Mendes <100716613+RafaelMendesMatias@users.noreply.github.com>
2025-04-12 01:45:28 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { ServerOptions } from './types/ServerOptions';
 
 export default {
   secretKey: 'wreducacional@85232025',
-  host: '0.0.0.0', ,
+  host: '0.0.0.0',
   port: '21465',
   deviceName: 'WREDUCACIONAL-SERVER',
   poweredBy: 'WREducacional',
